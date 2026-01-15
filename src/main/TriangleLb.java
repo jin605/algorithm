06499b868b79a3f3743b10ebd6aceee886ff8dc4
jinddd3 <jinddd3@gmail.com>
2026-01-15 23:02:09 +0900

    알고리즘 1주차

--- src/main/TriangleLb.java
+++ src/main/TriangleLb.java
@@ -8,7 +8,7 @@ public class TriangleLb {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("왼쪽 아래가 직각인 이등변 삼각형을 출력합니다.");
-        System.out.print("몇 단 삼각형입니까? :");
+        System.out.print("몇 단 삼각형입니까? : ");
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= n; i++){

2075bf6a55b3a36be9332709a37c697545426ee6
jinddd3 <jinddd3@gmail.com>
2026-01-26 00:30:53 +0900

    chap3

--- src/main/chap03/seqSearchSen.java
+++ src/main/chap03/seqSearchSen.java
@@ -38,6 +38,7 @@ public class seqSearchSen {
 
         if (idx == -1){
             System.out.println("그 값의 요소가 없습니다.");
+
         } else {
             System.out.printf("그 값은 x[%d]에 있습니다.",idx);
         }

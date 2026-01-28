c061609a649f69c8d796ad438fad5cffc5296662
jin605 <jinddd3@gmail.com>
2026-01-28 10:49:18 +0900

    ex commit

---
+++ src/main/ex/WhileEx1.java
@@ -0,0 +1,17 @@
+package main.ex;
+
+public class WhileEx1 {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
+
+        }
+
+
+    }
+}
---
+++ src/main/ex/WhileEx2.java
@@ -0,0 +1,16 @@
+package main.ex;
+
+public class WhileEx2 {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        while (count <= 10){
+            System.out.println(count*2);
+            count++;
+
+        }
+
+    }
+}
